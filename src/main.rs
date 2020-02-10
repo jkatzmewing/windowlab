a9ed22b178245e356c80a619771b5354e9d91704
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2020-02-10 10:39:20 -0500

    Beginnings of option handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate getopts;
+
+use getopts::Options;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let mut opts = Options::new();
+
+    opts.optopt("", "font", "set font", "FONT");
+    opts.optopt("", "border", "set border color", "COLOR");
+    opts.optopt("", "text", "set text color", "COLOR");
+    opts.optopt("", "active", "set active color", "COLOR");
+    opts.optopt("", "inactive", "set inactive color", "COLOR");
+    opts.optopt("", "menu", "set menu color", "COLOR");
+    opts.optopt("", "selected", "set selected color", "COLOR");
+    opts.optopt("", "empty", "set empty color", "COLOR");
+    opts.optopt("", "display", "set X11 display", "DISPLAY");
 }
