ccece5afaa06264579c889e45459b689d0e78c8c
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2020-02-10 10:53:59 -0500

    Start constructing skeleton of program

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ extern crate getopts;
 use getopts::Options;
 use std::env;
 
+mod events;
+mod menufile;
+mod taskbar;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -17,4 +21,18 @@ fn main() {
     opts.optopt("", "selected", "set selected color", "COLOR");
     opts.optopt("", "empty", "set empty color", "COLOR");
     opts.optopt("", "display", "set X11 display", "DISPLAY");
+
+    setup_display();
+    menufile::get_menuitems();
+    taskbar::make_taskbar();
+    scan_wins();
+    events::do_event_loop();
+}
+
+fn scan_wins() {
+    std::unimplemented!();
+}
+
+fn setup_display() {
+    std::unimplemented!();
 }
