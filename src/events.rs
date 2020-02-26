8948cb1e14d2ba2e05c1dd5b0bfb610dfeadcf03
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2020-02-26 17:24:29 -0500

    Add some more skeleton code to event loop

--- src/events.rs
+++ src/events.rs
@@ -36,14 +36,35 @@ pub fn do_event_loop(
                         xcb::cast_event(&ev)
                     };
                     handle_map_request(conn, root, state, ev);
-                }
+                },
 
                 xcb::UNMAP_NOTIFY => {
                     let ev: &xcb::UnmapNotifyEvent = unsafe {
                         xcb::cast_event(&ev)
                     };
                     handle_unmap_event(conn, root, state, ev);
-                }
+                },
+
+                xcb::DESTROY_NOTIFY => {
+                    let ev: &xcb::DestroyNotifyEvent = unsafe {
+                        xcb::cast_event(&ev)
+                    };
+                    handle_destroy_event(conn, root, state, ev);
+                },
+
+                xcb::CLIENT_MESSAGE => {
+                    let ev: &xcb::ClientMessageEvent = unsafe {
+                        xcb::cast_event(&ev)
+                    };
+                    handle_client_message(conn, root, state, ev);
+                },
+
+                xcb::COLORMAP_NOTIFY => {
+                    let ev: &xcb::ColormapNotifyEvent = unsafe {
+                        xcb::cast_event(&ev)
+                    };
+                    handle_colormap_change(conn, root, state, ev);
+                },
                 
                 _ => {}, // TODO other conditions
             }
@@ -92,6 +113,33 @@ fn handle_unmap_event(
     root: xcb::Window,
     state: &mut TotalState,
     ev: &xcb::UnmapNotifyEvent,
+) {
+    std::unimplemented!();
+}
+
+fn handle_destroy_event(
+    conn: &xcb::Connection,
+    root: xcb::Window,
+    state: &mut TotalState,
+    ev: &xcb::DestroyNotifyEvent,
+) {
+    std::unimplemented!();
+}
+
+fn handle_client_message(
+    conn: &xcb::Connection,
+    root: xcb::Window,
+    state: &mut TotalState,
+    ev: &xcb::ClientMessageEvent,
+) {
+    std::unimplemented!();
+}
+
+fn handle_colormap_change(
+    conn: &xcb::Connection,
+    root: xcb::Window,
+    state: &mut TotalState,
+    ev: &xcb::ColormapNotifyEvent,
 ) {
     std::unimplemented!();
 }
